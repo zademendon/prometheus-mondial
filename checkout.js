06805f9a00fbe09e5a6e19da527f27fd5b6026f0
zademendon <104177830+zademendon@users.noreply.github.com>
2023-06-05 17:22:07 -0300

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     const submitBtnEl = document.querySelector('#MaintContent_btnSubmitOrder');
-    const submitBtnEl.addEventListener('click', () => {
+    submitBtnEl.addEventListener('click', () => {
         const totalValue = document.querySelector('#MaintContent_lblUnitTotal').textContent;
         localStorage.setItem('totalValue', totalValue);
     });
